a062ae5072f386ed74851610b4fb3991b922aee8
Taliha <103150201+talihaq@users.noreply.github.com>
2023-01-18 03:42:53 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const heart = document.getElementById("heart");
+const navMenu = document.getElementById("navMenu");
+
+heart.addEventListener("click", () => {
+    navMenu.classList.toggle("show-menu");
+    navMenu.classList.contains("show-menu") ? document.getElementById("heart").style.position = "fixed" : document.getElementById("heart").style.position = "static";
+
+});
+
+window.onresize = function() {
+    // navMenu.classList.remove("show-menu");
+    if (window.outerWidth > 768) {
+        navMenu.classList.remove("show-menu");
+    }
+}
+
+function hideShowRequirements(val) {
+    if (val == "Y") {
+        document.getElementById("requirements").style.display = "block";
+        document.getElementById("requirements-label").style.display = "block";
+
+    } else {
+        document.getElementById("requirements").style.display = "none";
+        document.getElementById("requirements-label").style.display = "none";
+
+    }
+}
