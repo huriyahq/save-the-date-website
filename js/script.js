515604a384f02a1581d5be88458858fc56b26b88
Taliha <103150201+talihaq@users.noreply.github.com>
2023-01-18 04:19:55 +0000

    Update styles

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 const heart = document.getElementById("heart");
 const navMenu = document.getElementById("navMenu");
+// const hero = document.getElementById("hero");
 
 heart.addEventListener("click", () => {
     navMenu.classList.toggle("show-menu");
     navMenu.classList.contains("show-menu") ? document.getElementById("heart").style.position = "fixed" : document.getElementById("heart").style.position = "static";
 
+});
+
+window.addEventListener("load", () => {
+    document.getElementById("hero").style.backdropFilter = ""
 });
 
 window.onresize = function() {
